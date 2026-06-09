10dbc7b3714fbe6655e372866e58c69bf8e61957
Dac Vu <dacvu17@gmail.com>
2026-06-09 12:14:39 +0700

    Bump SharedKit to 0.17.0 (lenient Yob numeric decode)

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SharedKit",
-            url: "https://github.com/dacvn777sg/ios-shared-kit/releases/download/0.16.0/SharedKit-0.16.0.xcframework.zip",
-            checksum: "34d5743f590518ec418770ba7db0456a35ceba5bfc5b89974f8274a4c498dd64"
+            url: "https://github.com/dacvn777sg/ios-shared-kit/releases/download/0.17.0/SharedKit-0.17.0.xcframework.zip",
+            checksum: "c3ab88ac29097161d29820ce43f44c2e547aed48a060ae89ebccb77c7c4c32a8"
         )
     ]
 )
