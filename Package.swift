41394b3fc0a5c686c1fae14f04270c146779a548
Dac Vu <dacvu17@gmail.com>
2026-06-09 11:55:15 +0700

    Bump SharedKit to 0.16.0 (getCurrentUser direct shape)

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SharedKit",
-            url: "https://github.com/dacvn777sg/ios-shared-kit/releases/download/0.15.0/SharedKit-0.15.0.xcframework.zip",
-            checksum: "5391e0f2b4929700b5085b6da0a22a79ec3aedc964790e1ffc5c074187b2d54d"
+            url: "https://github.com/dacvn777sg/ios-shared-kit/releases/download/0.16.0/SharedKit-0.16.0.xcframework.zip",
+            checksum: "34d5743f590518ec418770ba7db0456a35ceba5bfc5b89974f8274a4c498dd64"
         )
     ]
 )
