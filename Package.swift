9de8ffd520ffd3bbc7842abc64193d67fb9ad2a8
Dac Vu <dacvu17@gmail.com>
2026-06-09 11:35:06 +0700

    Bump SharedKit to 0.15.0 (Ktor Logging debug build)

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SharedKit",
-            url: "https://github.com/dacvn777sg/ios-shared-kit/releases/download/0.14.0/SharedKit-0.14.0.xcframework.zip",
-            checksum: "eecb7c744aee5ad107a36ac8511ee2921910aeceb901ea534053b544175d3345"
+            url: "https://github.com/dacvn777sg/ios-shared-kit/releases/download/0.15.0/SharedKit-0.15.0.xcframework.zip",
+            checksum: "5391e0f2b4929700b5085b6da0a22a79ec3aedc964790e1ffc5c074187b2d54d"
         )
     ]
 )
